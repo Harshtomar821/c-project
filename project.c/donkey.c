65e38d46c0f1ee05f4d5d3593bf7aca93cc7de00
Harshtomar821 <ht716756@gmail.com>
2024-08-28 16:10:43 +0530

    it's a c programming based project it consist a game ,it's basically a quiz game  if we you give the right answers than you move to next level, if you choose the wrong answer than u lose the game and you come out the game with the score card

---
+++ project.c/donkey.c
@@ -0,0 +1,293 @@
+#include<stdio.h>
+int main(){
+    int a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z;
+     printf("thanks for visit my game\n");
+     printf("sir if you want to start the game please enter 9\n");
+    // printf("if you quit the ")
+    scanf("%d",&a);
+    if(a==9){
+        printf("thanks sir for start my game\n  ");
+    }
+    else{
+        printf("sir you quit the game please try again\n");
+
+    }
+    if(a==9){
+        printf(" what is meaning of nostelgia\n ");
+        printf("1.a old mindset man\n");
+        printf("2.a new mindset people\n");
+        printf("3.a moderate mindset\n");
+        printf("4.a highly brialliant man\n");
+        scanf("%d",&b);
+        if(b==1){
+            printf("\nright answer sir\n");
+            printf("we move to second question\n\n");
+            printf("what is the antonym of open\n");
+            printf(" 1. turn\n");
+            printf("2.close\n");
+            printf("3.move\n");
+            printf("4.sleep\n");
+            scanf("%d",&c);
+            if(c==2){
+                printf("\n right answer sir\n");
+                printf("we move to another  question\n\n");
+                printf(" charge is discovered by\n");
+                printf("1.newton\n");
+                printf("2.franklin\n");
+                printf("3.paradox\n");
+                printf("4.maxwel\n");
+                scanf("%d",&d);
+                if(d==2){
+                    printf("\nright answer sir \n");
+                    printf("we move to another question\n\n");
+                    printf(" owl comes under the catagory of ??\n");
+                    printf("1.homosipians\n");
+                    printf("2.trizopiums\n");
+                    printf("3.drums\n");
+                    printf("4.strigiforms\n");
+                    scanf("%d",&e);
+                    if(e==4){
+                        printf(" \nright answer sir\n ");
+                        printf(" who is the author of power of subscouncious mind\n");
+                        printf("1.robert fost\n");
+                        printf("2.rudyaqrd kiplin\n");
+                        printf("3.joshef merfi\n");
+                        printf("4.engilla bell\n");
+                        scanf("%d",&f);
+                        if(f==3){
+                            printf(" \nright anwer sir we move to another question\n");
+                            printf(" the animals which kill another animals are called\n ");
+                            printf(" 1. doplers\n");
+                            printf("2.curdors\n");
+                            printf("3.predators\n");
+                            printf("4.homoshopians\n");
+                            scanf("%d",&g);
+                            if(g==3){
+                                printf(" \nright answer sir\n ");
+                                printf(" who is the father of computer\n\n");
+                                printf("1.madam cury\n");
+                                printf("2.sir jems rod\n");
+                                printf("3.charls babbage\n");
+                                printf("4.vicky kousal\n");
+                                scanf("%d",&h);
+                                if(h==3){
+                                printf(" \nright answer sir\n\n ");
+                                printf(" we move to another question\n");
+                                printf(" who is the father of atomic bomb\n");
+                                printf("1.oppen heimer\n");
+                                printf("2.rodrik bills\n");
+                                printf("3.maxwell\n");
+                                printf("4.dr doom\n");
+                                scanf("%d",&i);
+                                if(i==1){
+                                    printf("\n brilliant sir\n");
+                                    printf(" right answer sir\n ");
+                                    printf(" we move to another question\n");
+                                    printf(" who is the ceo of google\n\n");
+                                    printf("1. harshu\n ");
+                                    printf("2.sundar pichai\n");
+                                    printf("3.bill gates\n");
+                                    printf("4.jef bezos\n");
+                                    scanf("%d",&j);
+                                    if(j==2){
+                                        printf("\n\n exelent sir\n");
+                                        printf(" we move to another question\n");
+                                        printf(" who is the senior citizen of india\n");
+                                        printf("1.prime minister\n");
+                                        printf("2.cheif minister\n");
+                                        printf("3.president\n");
+                                        printf("4.MLA\n");
+                                        scanf("%d",&k);
+                                        if(k==3){
+                                            printf(" \nwow sir you are in last level\n");
+                                            printf(" we move to another question\n");
+                                            printf(" world richest man is \n");
+                                            printf("1. elon musk\n");
+                                            printf("2. jef bejos\n");
+                                            printf("3.mark jukerberg\n");
+                                            printf("4.bill gates\n");
+                                            scanf("%d",&l);
+                                            if(l==1){
+                                                printf(" \nwell done sir \n");
+                                                printf(" we move to another question\n");
+                                                printf(" who kill mahatma gandhi\n");
+                                                printf("1.harshu\n");
+                                                printf("2.Nathu ram godse\n");
+                                                printf("3. trevis head\n");
+                                                printf("4.sonu gunda\n");
+                                                scanf("%d",&m);
+                                                if(m==2){
+                                                    printf("\n good job sir\n");
+                                                    printf(" we move to another question\n");
+                                                    printf(" who is written indian constitution\n");
+                                                    printf("1. anshuman bhadoriya\n");
+                                                    printf("2. dr bheem rao ambedkar\n");
+                                                    printf("3. bill gates\n");
+                                                    printf("4.modi\n");
+                                                    scanf("%d",&n);
+                                                    if(n==2){
+                                                        printf("\nCONGRATULATIONS SIR\n ");
+                                                        printf(" we are in next level\n");
+                                                        printf(" so the question is \n");
+                                                        printf(" who is founder of aazad hind foj\n");
+                                                        printf("1. nathu ram\n");
+                                                        printf("2. pappu yadav\n");
+                                                        printf("3. ram prasad bismil\n");
+                                                        printf("4. subhash chandra boss\n");
+                                                        scanf("%d",&o);
+                                                        if(o==4){
+                                                            printf(" \nright answer sir\n ");
+                                                            printf(" we are in phase of game end\n");
+                                                            printf(" which of following are types of rock\n");
+                                                            printf("1.mulberry or rose\n");
+                                                            printf("2. ignious or sendimentry\n");
+                                                            printf("3.venue or kenda\n ");
+                                                            printf("4.motal or phenal\n");
+                                                            scanf("%d",&p);
+                                                            if(p==2){
+                                                                printf("\nright answer sir\n");
+                                                                printf(" we move to second question\n");
+                                                                printf("world largest river\n  ");
+                                                                printf("1.amazon\n");
+                                                                printf("2.neel\n");
+                                                                printf("3.ganga\n");
+                                                                printf("4.bramputra\n");
+                                                                scanf("%d",&q);
+                                                                if(q==2){
+                                                                    printf("\n world largest forest is\n ");
+                                                                    printf("1.amazon\n");
+                                                                    printf("2. panchmani\n");
+                                                                    printf("3. kakori\n");
+                                                                    printf("4.lelua\n");
+                                                                    scanf("%d",&r);
+                                                                    if(r==1){
+                                                                        printf("\nsir good job only one question is left for voctory\n");
+                                                                        printf(" the last question is\n ");
+                                                                        printf("the isro is establish in the year of\n ");
+                                                                        printf("1.1984\n");
+                                                                        printf("2.1989\n");
+                                                                        printf("3.1969\n");
+                                                                        printf("4.1970\n");
+                                                                        scanf("%d",&s);
+                                                                        if(s==3){
+                                                                            printf(" \n99 CONGRATULATIONS SIR YOU WON THE MATCH\n");
+                                                                            printf("YOUR SCORE IS BEST\n");
+                                                                            printf(" HURRRAY\n");
+                                                                            
+                                                                        
+                                                                        }
+                                                                        else{
+                                                                            printf(" sorry sir you loose the match \n ");
+                                                                            printf(" your score is 17\n\n");
+
+                                                                        }
+                                                                    }
+                                                                    else{
+                                                                        printf(" srry sir you loose the game \n");
+                                                                        printf(" your score is 16\n\n");
+                                                                    }
+                                                                }
+                                                                else{
+                                                                    printf(" srry sir you loose the game\n ");
+                                                                    printf(" yoUR SCORE IS 15\n");
+                                                                }
+                                                            }
+                                                            else{
+                                                                printf(" SRRY SIR YOU LOOSE THE GAME\n");
+                                                                printf(" YOUR SCORE IS 14\n");
+                                                            }
+                                                        }
+                                                        else{
+                                                                printf("SRRY SIR YOU LOOSE THE GAME\n");
+                                                                printf(" YOUR SCORE IS 13\n");
+                                                            }
+                                                    }
+                                                    else{
+                                                                printf("SRRY SIR YOU LOOSE THE GAME\n");
+                                                                printf(" YOUR SCORE IS 12\n");
+                                                            }
+                                                }
+                                                else{
+                                                                printf("SRRY SIR YOU LOOSE THE GAME\n");
+                                                                printf(" YOUR SCORE IS 11\n");
+                                                            }
+                                            }
+                                            else{
+                                                                printf("SRRY SIR YOU LOOSE THE GAME\n");
+                                                                printf(" YOUR SCORE IS 10\n");
+                                                            }
+
+                                            
+
+                                        }
+                                        else{
+                                                                printf("SRRY SIR YOU LOOSE THE GAME\n");
+                                                                printf(" YOUR SCORE IS 9\n");
+                                                            }
+                                    }
+                                    else{
+                                                                printf("SRRY SIR YOU LOOSE THE GAME\n");
+                                                                printf(" YOUR SCORE IS 8\n");
+                                                            }
+                                    
+                                    
+                                    
+                                    
+                                }
+                                else{
+                                                                printf("SRRY SIR YOU LOOSE THE GAME\n");
+                                                                printf(" YOUR SCORE IS 7\n");
+                                                            }
+                                }
+                                else{
+                                                                printf("SRRY SIR YOU LOOSE THE GAME\n");
+                                                                printf(" YOUR SCORE IS 6\n");
+                                                            }
+
+                            }
+                            else{
+                                                                printf("SRRY SIR YOU LOOSE THE GAME\n");
+                                                                printf(" YOUR SCORE IS 5\n");
+                                                            }
+                            
+                            
+                            
+                        }
+                        else{
+                                                                printf("SRRY SIR YOU LOOSE THE GAME\n");
+                                                                printf(" YOUR SCORE IS 4\n");
+                                                            }
+                    }
+                    else{
+                                                                printf("SRRY SIR YOU LOOSE THE GAME\n");
+                                                                printf(" YOUR SCORE IS 3\n");
+                                                            }
+                }
+                else{
+                                                                printf("SRRY SIR YOU LOOSE THE GAME\n");
+                                                                printf(" YOUR SCORE IS 2\n");
+                                                            }
+
+            }
+            else{
+                                                                printf("SRRY SIR YOU LOOSE THE GAME\n");
+                                                                printf(" YOUR SCORE IS 1\n");
+                                                            }
+            
+        }
+        else{
+                                                                printf("SRRY SIR YOU LOOSE THE GAME");
+                                                                printf(" YOUR SCORE IS 0");
+                                                            }
+        
+    
+    }
+    
+
+    
+     
+    
+
+    }
+
+
